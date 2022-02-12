803a1808fc0467b947f33776942844f99f803bd4
Khushboo Shah <shahkhushboo0@gmail.com>
2022-02-12 12:28:02 -0500

    Initial commit for set up

---
+++ src/main/java/com/personal/project/clockinclockout/ClockInClockOutApplication.java
@@ -0,0 +1,13 @@
+package com.personal.project.clockinclockout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockInClockOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockInClockOutApplication.class, args);
+	}
+
+}
